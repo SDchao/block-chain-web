bd8496608e72e4834494818eecbbeb0959730936
SDchao <wentaochens@outlook.com>
2021-01-26 16:21:35 +0800

    manager init

---
+++ src/views/Login.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="login">
+    <el-container direction="vertical">
+      <h1>系统登录</h1>
+      <el-input
+        class="input"
+        v-model="username_input"
+        placeholder="用户名"
+      ></el-input>
+      <el-input
+        class="input"
+        v-model="passwd_input"
+        placeholder="密码"
+        show-password
+      ></el-input>
+      <el-checkbox v-model="remember_checkbox" label="记住密码"></el-checkbox>
+      <div class="button_group">
+        <el-button
+          type="primary"
+          icon="el-icon-user-solid"
+          @click="onLoginClicked"
+          >登录</el-button
+        >
+        <el-button icon="el-icon-s-check">注册</el-button>
+      </div>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      username_input: "",
+      passwd_input: "",
+      remember_checkbox: false,
+    };
+  },
+  methods: {
+    onLoginClicked() {
+      console.log("Submit " + this.username_input + " " + this.passwd_input);
+      window.location.replace("/Manage");
+    },
+  },
+};
+</script>
+
+<style scoped>
+#login {
+  text-align: center;
+  margin: 8px 0;
+}
+
+.input {
+  max-width: 300px;
+  margin: 5px auto;
+}
+
+.el-checkbox {
+  margin: 5px;
+}
+
+.el-button {
+  margin: 5px;
+}
+</style>
---
+++ src/views/Manage.vue
@@ -0,0 +1,60 @@
+<template>
+    <div id="Manage">
+        <el-container>
+            <el-header>
+                <h1>区块链学历资质管理系统</h1>
+            </el-header>
+
+            <el-aside width="250px">
+                <h5>管理事项</h5>
+                <el-menu default-active="0-0" @select="handleSelect">
+                    <el-submenu index="0">
+                        <template slot="title">
+                            <i class="el-icon-menu"></i>
+                            <span>测试0</span>
+                        </template>
+                        <el-menu-item-group>
+                            <el-menu-item index="0-0">0-0</el-menu-item>
+                            <el-menu-item index="0-1">0-1</el-menu-item>
+                        </el-menu-item-group>
+                    </el-submenu>
+                </el-menu>
+            </el-aside>
+            <el-main>
+                <router-view> </router-view>
+            </el-main>
+        </el-container>
+    </div>
+</template>
+
+<style scoped>
+.el-header {
+    background-color: black;
+    color: black;
+    margin: 0;
+}
+
+h5 {
+    text-align: center;
+}
+
+h1 {
+    text-align: left;
+    background: black;
+    color: white;
+}
+</style>
+
+<script>
+export default {
+    name: "Manage",
+    data() {
+        return {};
+    },
+    methods: {
+        handleSelect(key, keyPath) {
+            console.log(key, keyPath);
+        },
+    },
+};
+</script>
